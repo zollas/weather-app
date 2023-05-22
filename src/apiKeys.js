fae3206f9b773af752a7b25e1f8607220eafb910
zollas <ns318428@gmail.com>
2023-05-22 23:58:53 +0530

    neha deep thinker

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "e2438f6da9af12f2488b1c938ab459f8",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
